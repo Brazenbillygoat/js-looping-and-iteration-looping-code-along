59f02069eba1cad7a78fd599b6096e946c3e1ba0
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-10-06 08:15:26 -0400

    Wrote a while and a for loop.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+function writeCards(nameArray, eventName) {
+  const thankYous = []
+
+  for (i = 0; i < nameArray.length; i++) {
+    thankYous.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`)
+  }
+
+  return thankYous
+}
+
+writeCards(["Turkey", "Gizzard"], "thanksgiving")
+
+
+function countDown(start) {
+  let count = start;
+
+  while (count >= 0) {
+    console.log(count)
+    count -= 1;
+  }
+
+}
+
+countDown(10)
